2de8ef82c357de218b0edde4a98a4a57304a02c9
Pozitronik <antikillerxxl@gmail.com>
2021-08-12 19:30:03 +0300

    Единый codestyle

--- src/Jwt.php
+++ src/Jwt.php
@@ -35,224 +35,225 @@ use yii\di\Instance;
  */
 class Jwt extends Component
 {
-	public const HS256 = Signer\Hmac\Sha256::class;
-	public const HS384 = Signer\Hmac\Sha384::class;
-	public const HS512 = Signer\Hmac\Sha512::class;
-	public const RS256 = Signer\Rsa\Sha256::class;
-	public const RS384 = Signer\Rsa\Sha384::class;
-	public const RS512 = Signer\Rsa\Sha512::class;
-	public const ES256 = Signer\Ecdsa\Sha256::class;
-	public const ES384 = Signer\Ecdsa\Sha384::class;
-	public const ES512 = Signer\Ecdsa\Sha512::class;
+    public const HS256 = Signer\Hmac\Sha256::class;
+    public const HS384 = Signer\Hmac\Sha384::class;
+    public const HS512 = Signer\Hmac\Sha512::class;
+    public const RS256 = Signer\Rsa\Sha256::class;
+    public const RS384 = Signer\Rsa\Sha384::class;
+    public const RS512 = Signer\Rsa\Sha512::class;
+    public const ES256 = Signer\Ecdsa\Sha256::class;
+    public const ES384 = Signer\Ecdsa\Sha384::class;
+    public const ES512 = Signer\Ecdsa\Sha512::class;
 
-	public const ALGORITHM_TYPE_SYMMETRIC  = 'symmetric';
-	public const ALGORITHM_TYPE_ASYMMETRIC = 'asymmetric';
+    public const ALGORITHM_TYPE_SYMMETRIC = 'symmetric';
+    public const ALGORITHM_TYPE_ASYMMETRIC = 'asymmetric';
 
-	/**
-	 * @var array Algorithm types.
-	 */
-	public array $algorithmTypes = [
-		self::HS256 => self::ALGORITHM_TYPE_SYMMETRIC,
-		self::HS384 => self::ALGORITHM_TYPE_SYMMETRIC,
-		self::HS512 => self::ALGORITHM_TYPE_SYMMETRIC,
-		self::RS256 => self::ALGORITHM_TYPE_ASYMMETRIC,
-		self::RS384 => self::ALGORITHM_TYPE_ASYMMETRIC,
-		self::RS512 => self::ALGORITHM_TYPE_ASYMMETRIC,
-		self::ES256 => self::ALGORITHM_TYPE_ASYMMETRIC,
-		self::ES384 => self::ALGORITHM_TYPE_ASYMMETRIC,
-		self::ES512 => self::ALGORITHM_TYPE_ASYMMETRIC,
-	];
+    /**
+     * @var array Algorithm types.
+     */
+    public array $algorithmTypes = [
+        self::HS256 => self::ALGORITHM_TYPE_SYMMETRIC,
+        self::HS384 => self::ALGORITHM_TYPE_SYMMETRIC,
+        self::HS512 => self::ALGORITHM_TYPE_SYMMETRIC,
+        self::RS256 => self::ALGORITHM_TYPE_ASYMMETRIC,
+        self::RS384 => self::ALGORITHM_TYPE_ASYMMETRIC,
+        self::RS512 => self::ALGORITHM_TYPE_ASYMMETRIC,
+        self::ES256 => self::ALGORITHM_TYPE_ASYMMETRIC,
+        self::ES384 => self::ALGORITHM_TYPE_ASYMMETRIC,
+        self::ES512 => self::ALGORITHM_TYPE_ASYMMETRIC,
+    ];
 
-	/**
-	 * @var string|array|Signer|null конфигурация компонента подписи.
-	 * Если задано `null` значение, то будет использована unsecured конфигурация.
-	 */
-	public $signer = self::HS256;
-	/**
-	 * @var string|array|Key конфигурация ключа подписи.
-	 */
-	public $signerKey = '';
-	/**
-	 * @var string|array|Key конфигурация ключа верификации.
-	 * При использовании симметричного алгоритма подписи свойство игнорируется.
-	 */
-	public $verifyKey = '';
-	/**
-	 * @var string|array|Encoder Custom encoder.
-	 */
-	public $encoder = JoseEncoder::class;
-	/**
-	 * @var string|array|Decoder Custom decoder.
-	 */
-	public $decoder = JoseEncoder::class;
-	/**
-	 * @var array|Constraint[] список валидаторов для проверки токена.
-	 * Требуется задать, как минимум, один валидатор.
-	 * @see configureConstraints()
-	 */
-	public array $validationConstraints = [];
+    /**
+     * @var string|array|Signer|null конфигурация компонента подписи.
+     * Если задано `null` значение, то будет использована unsecured конфигурация.
+     */
+    public $signer = self::HS256;
+    /**
+     * @var string|array|Key конфигурация ключа подписи.
+     */
+    public $signerKey = '';
+    /**
+     * @var string|array|Key конфигурация ключа верификации.
+     * При использовании симметричного алгоритма подписи свойство игнорируется.
+     */
+    public $verifyKey = '';
+    /**
+     * @var string|array|Encoder Custom encoder.
+     */
+    public $encoder = JoseEncoder::class;
+    /**
+     * @var string|array|Decoder Custom decoder.
+     */
+    public $decoder = JoseEncoder::class;
+    /**
+     * @var array|Constraint[] список валидаторов для проверки токена.
+     * Требуется задать, как минимум, один валидатор.
+     * @see configureConstraints()
+     */
+    public array $validationConstraints = [];
 
-	/**
-	 * @var Configuration|null
-	 */
-	private ?Configuration $_configuration = null;
+    /**
+     * @var Configuration|null
+     */
+    private ?Configuration $_configuration = null;
 
-	/**
-	 * @throws InvalidConfigException
-	 */
-	public function init(): void
-	{
-		parent::init();
+    /**
+     * @throws InvalidConfigException
+     */
+    public function init():void
+    {
+        parent::init();
 
-		Yii::$container->set(Clock::class, static fn() => SystemClock::fromSystemTimezone());
+        Yii::$container->set(Clock::class, static fn() => SystemClock::fromSystemTimezone());
 
-		$this->initEncoders();
-		if ($this->signer === null) {
-			$this->_configuration = Configuration::forUnsecuredSigner($this->encoder, $this->decoder);
-		} else {
-			$this->initSigner();
-			$this->initKey($this->signerKey);
-			$this->initKey($this->verifyKey);
+        $this->initEncoders();
+        if ($this->signer === null) {
+            $this->_configuration = Configuration::forUnsecuredSigner($this->encoder, $this->decoder);
+        } else {
+            $this->initSigner();
+            $this->initKey($this->signerKey);
+            $this->initKey($this->verifyKey);
 
-			$this->initConfiguration();
-		}
-	}
+            $this->initConfiguration();
+        }
+    }
 
-	/**
-	 * @param string $jwt
-	 * @return UnencryptedToken
-	 */
-	public function parse(string $jwt): UnencryptedToken
-	{
-		return $this->parser->parse($jwt);
-	}
+    /**
+     * @param string $jwt
+     * @return UnencryptedToken
+     */
+    public function parse(string $jwt):UnencryptedToken
+    {
+        return $this->parser->parse($jwt);
+    }
 
-	/**
-	 * @return Configuration
-	 */
-	public function getConfiguration(): Configuration
-	{
-		return $this->_configuration;
-	}
+    /**
+     * @return Configuration
+     */
+    public function getConfiguration():Configuration
+    {
+        return $this->_configuration;
+    }
 
-	/**
-	 * @param ClaimsFormatter|null $claimFormatter
-	 * @return Builder
-	 */
-	public function getBuilder(?ClaimsFormatter $claimFormatter = null): Builder
-	{
-		return $this->_configuration->builder($claimFormatter);
-	}
+    /**
+     * @param ClaimsFormatter|null $claimFormatter
+     * @return Builder
+     */
+    public function getBuilder(?ClaimsFormatter $claimFormatter = null):Builder
+    {
+        return $this->_configuration->builder($claimFormatter);
+    }
 
-	/**
-	 * @return Parser
-	 */
-	public function getParser(): Parser
-	{
-		return $this->_configuration->parser();
-	}
+    /**
+     * @return Parser
+     */
+    public function getParser():Parser
+    {
+        return $this->_configuration->parser();
+    }
 
-	/**
-	 * @param Token $token
-	 */
-	public function assert(Token $token): void
-	{
-		$this->_configuration->validator()->assert($token, ...$this->_configuration->validationConstraints());
-	}
+    /**
+     * @param Token $token
+     */
+    public function assert(Token $token):void
+    {
+        $this->_configuration->validator()->assert($token, ...$this->_configuration->validationConstraints());
+    }
 
-	/**
-	 * @param Token $token
-	 * @return bool
-	 */
-	public function validate(Token $token): bool
-	{
-		return $this->_configuration->validator()->validate($token, ...$this->_configuration->validationConstraints());
-	}
+    /**
+     * @param Token $token
+     * @return bool
+     */
+    public function validate(Token $token):bool
+    {
+        return $this->_configuration->validator()->validate($token, ...$this->_configuration->validationConstraints());
+    }
 
-	/**
-	 * @throws InvalidConfigException
-	 */
-	private function initConfiguration(): void
-	{
-		$algorithm = $this->algorithmTypes[get_class($this->signer)] ?? null;
+    /**
+     * @throws InvalidConfigException
+     */
+    private function initConfiguration():void
+    {
+        $algorithm = $this->algorithmTypes[get_class($this->signer)]??null;
 
-		if ($algorithm === self::ALGORITHM_TYPE_SYMMETRIC) {
-			$this->_configuration = Configuration::forSymmetricSigner(
-				$this->signer,
-				$this->signerKey,
-				$this->encoder,
-				$this->decoder
-			);
-		} elseif ($algorithm === self::ALGORITHM_TYPE_ASYMMETRIC) {
-			$this->_configuration = Configuration::forAsymmetricSigner(
-				$this->signer,
-				$this->signerKey,
-				$this->verifyKey,
-				$this->encoder,
-				$this->decoder
-			);
-		} else {
-			throw new InvalidConfigException('Invalid signer ID!');
-		}
+        if ($algorithm === self::ALGORITHM_TYPE_SYMMETRIC) {
+            $this->_configuration = Configuration::forSymmetricSigner(
+                $this->signer,
+                $this->signerKey,
+                $this->encoder,
+                $this->decoder
+            );
+        } elseif ($algorithm === self::ALGORITHM_TYPE_ASYMMETRIC) {
+            $this->_configuration = Configuration::forAsymmetricSigner(
+                $this->signer,
+                $this->signerKey,
+                $this->verifyKey,
+                $this->encoder,
+                $this->decoder
+            );
+        } else {
+            throw new InvalidConfigException('Invalid signer ID!');
+        }
 
-		$this->configureConstraints();
-	}
+        $this->configureConstraints();
+    }
 
-	/**
-	 * @throws InvalidConfigException
-	 */
-	private function initSigner(): void
-	{
-		//применимо только для Ecdsa
-		Yii::$container->set(SignatureConverter::class, MultibyteStringConverter::class);
+    /**
+     * @throws InvalidConfigException
+     */
+    private function initSigner():void
+    {
+        //применимо только для Ecdsa
+        Yii::$container->set(SignatureConverter::class, MultibyteStringConverter::class);
 
-		$this->signer = Instance::ensure($this->signer, Signer::class);
-	}
+        $this->signer = Instance::ensure($this->signer, Signer::class);
+    }
 
-	/**
-	 * @throws InvalidConfigException
-	 */
-	private function initEncoders(): void
-	{
-		$this->encoder = Instance::ensure($this->encoder, Encoder::class);
-		$this->decoder = Instance::ensure($this->decoder, Encoder::class);
-	}
+    /**
+     * @throws InvalidConfigException
+     */
+    private function initEncoders():void
+    {
+        $this->encoder = Instance::ensure($this->encoder, Encoder::class);
+        $this->decoder = Instance::ensure($this->decoder, Encoder::class);
+    }
 
-	/**
-	 * @param mixed $key
-	 * @throws InvalidConfigException
-	 */
-	private function initKey(&$key): void
-	{
-		if ('' === $key) {
-			return;
-		}
+    /**
+     * @param mixed $key
+     * @throws InvalidConfigException
+     */
+    private function initKey(&$key):void
+    {
+        if ('' === $key) {
+            return;
+        }
 
-		if (is_string($key)) {
-			if (strpos($key, '@') === 0) {
-				$key = LocalFileReference::file(Yii::getAlias($key));
-			} elseif (strpos($key, 'file://') === 0) {
-				$key = LocalFileReference::file($key);
-			} else {
-				$key = InMemory::plainText($key);
-			}
-		} else {
-			$key = Instance::ensure($key, Key::class);
-		}
-	}
+        if (is_string($key)) {
+            if (strpos($key, '@') === 0) {
+                $key = LocalFileReference::file(Yii::getAlias($key));
+            } elseif (strpos($key, 'file://') === 0) {
+                $key = LocalFileReference::file($key);
+            } else {
+                $key = InMemory::plainText($key);
+            }
+        } else {
+            $key = Instance::ensure($key, Key::class);
+        }
+    }
 
-	/**
-	 * @throws InvalidConfigException
-	 */
-	private function configureConstraints(): void
-	{
-		$constraints = array_map(
-			static function ($constraint) {
-				return Instance::ensure($constraint, Constraint::class);
-			},
-			$this->validationConstraints
-		);
+    /**
+     * @throws InvalidConfigException
+     */
+    private function configureConstraints():void
+    {
+        $constraints = array_map(
+            static function($constraint)
+            {
+                return Instance::ensure($constraint, Constraint::class);
+            },
+            $this->validationConstraints
+        );
 
-		$this->_configuration->setValidationConstraints(...$constraints);
-	}
+        $this->_configuration->setValidationConstraints(...$constraints);
+    }
 }
--- src/JwtHttpBearerAuth.php
+++ src/JwtHttpBearerAuth.php
@@ -22,40 +22,40 @@ use yii\web\User;
  */
 class JwtHttpBearerAuth extends HttpBearerAuth
 {
-	/**
-	 * @var string|array|Jwt id компонента, его конфигурация или сам объект компонента.
-	 */
+    /**
+     * @var string|array|Jwt id компонента, его конфигурация или сам объект компонента.
+     */
     public $jwt = 'jwt';
-	/**
-	 * @var string токен (payload-параметр), использующийся для идентификации пользователя в системе.
-	 * По умолчанию - jti (JWT ID).
-	 */
-	public string $identifyClaim = 'jti';
-	/**
-	 * @var Closure|null анонимная функция, возвращающая пользовательскую конфигарацию компонента jwt.
-	 * Если конфигурация не задана, компонент будет использовать настройки по умолчанию.
-	 */
+    /**
+     * @var string токен (payload-параметр), использующийся для идентификации пользователя в системе.
+     * По умолчанию - jti (JWT ID).
+     */
+    public string $identifyClaim = 'jti';
+    /**
+     * @var Closure|null анонимная функция, возвращающая пользовательскую конфигарацию компонента jwt.
+     * Если конфигурация не задана, компонент будет использовать настройки по умолчанию.
+     */
     public ?Closure $jwtOptionsCallback = null;
 
     /**
-	 * {@inheritdoc}
+     * {@inheritdoc}
      * @throws InvalidConfigException
      */
-    public function init(): void
+    public function init():void
     {
         parent::init();
 
         $this->jwt = Instance::ensure($this->jwt, Jwt::class);
-        
+
         if (empty($this->pattern)) {
             throw new InvalidConfigException('You must provide pattern to use to extract the HTTP authentication value!');
         }
     }
 
-	/**
-	 * {@inheritdoc}
-	 */
-    public function authenticate($user, $request, $response): ?IdentityInterface
+    /**
+     * {@inheritdoc}
+     */
+    public function authenticate($user, $request, $response):?IdentityInterface
     {
         $authHeader = $request->getHeaders()->get($this->header);
 
@@ -66,74 +66,74 @@ class JwtHttpBearerAuth extends HttpBearerAuth
         try {
             $token = $this->jwt->parse($matches[1]);
         } catch (Throwable $e) {
-			$token = null;
+            $token = null;
             $this->fail($response, $e);
         }
 
-		$identity = $this->getIdentity($token, $user);
-		if (null === $identity) {
-			$this->fail($response);
-		}
+        $identity = $this->getIdentity($token, $user);
+        if (null === $identity) {
+            $this->fail($response);
+        }
 
-		try {
-			$this->configureUserOptions($identity);
+        try {
+            $this->configureUserOptions($identity);
 
-			$validateIsOk = $this->jwt->validate($token);
-		} catch (Throwable $e) {
-			$validateIsOk = false;
-			$this->fail($response, $e);
-		}
+            $validateIsOk = $this->jwt->validate($token);
+        } catch (Throwable $e) {
+            $validateIsOk = false;
+            $this->fail($response, $e);
+        }
 
-		if (!$validateIsOk || !$user->login($identity)) {
-			$this->fail($response);
-		}
+        if (!$validateIsOk || !$user->login($identity)) {
+            $this->fail($response);
+        }
 
         return $identity;
     }
 
-	/**
-	 * @param UnencryptedToken $token
-	 * @param User $user
-	 * @return IdentityInterface|null
-	 */
-    private function getIdentity(UnencryptedToken $token, User $user): ?IdentityInterface
-	{
-		$identifyClaim = $token->claims()->get($this->identifyClaim);
-		if (null === $identifyClaim) {
-			return null;
-		}
-
-		/** @var IdentityInterface $class */
-		$class = $user->identityClass;
-
-		return $class::findIdentityByAccessToken($identifyClaim, static::class);
-	}
-
-	/**
-	 * @param IdentityInterface $user
-	 * @throws InvalidConfigException
-	 */
-	private function configureUserOptions(IdentityInterface $user): void
-	{
-		if (null !== $this->jwtOptionsCallback) {
-			$userOptions = call_user_func($this->jwtOptionsCallback, $user);
-			if (is_array($userOptions) && ([] !== $userOptions)) {
-				Yii::configure($this->jwt, $userOptions);
-				$this->jwt->init();
-			}
-		}
-	}
-
-	/**
-	 * @param Response $response
-	 * @param Throwable|null $e
-	 * @throws UnauthorizedHttpException
-	 */
-    private function fail(Response $response, ?Throwable $e = null): void
+    /**
+     * @param UnencryptedToken $token
+     * @param User $user
+     * @return IdentityInterface|null
+     */
+    private function getIdentity(UnencryptedToken $token, User $user):?IdentityInterface
     {
-    	if (null !== $e) {
-			Yii::error($e, static::class);
-		}
+        $identifyClaim = $token->claims()->get($this->identifyClaim);
+        if (null === $identifyClaim) {
+            return null;
+        }
+
+        /** @var IdentityInterface $class */
+        $class = $user->identityClass;
+
+        return $class::findIdentityByAccessToken($identifyClaim, static::class);
+    }
+
+    /**
+     * @param IdentityInterface $user
+     * @throws InvalidConfigException
+     */
+    private function configureUserOptions(IdentityInterface $user):void
+    {
+        if (null !== $this->jwtOptionsCallback) {
+            $userOptions = call_user_func($this->jwtOptionsCallback, $user);
+            if (is_array($userOptions) && ([] !== $userOptions)) {
+                Yii::configure($this->jwt, $userOptions);
+                $this->jwt->init();
+            }
+        }
+    }
+
+    /**
+     * @param Response $response
+     * @param Throwable|null $e
+     * @throws UnauthorizedHttpException
+     */
+    private function fail(Response $response, ?Throwable $e = null):void
+    {
+        if (null !== $e) {
+            Yii::error($e, static::class);
+        }
 
         $this->challenge($response);
         $this->handleFailure($response);
